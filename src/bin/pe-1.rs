ebec98b2a2f4cc9c1b5dbf3c747ceee01712b27f
Andreas Christianson <achristianson@pessimistic-it.com>
2021-03-01 00:38:47 -0600

    formatting

--- src/bin/pe-1.rs
+++ src/bin/pe-1.rs
@@ -1,5 +1,5 @@
 fn main() {
     println!("pe #1");
-    let sum: i32 = (0..1000).filter(|n| n%3 == 0 || n%5 == 0).sum();
+    let sum: i32 = (0..1000).filter(|n| n % 3 == 0 || n % 5 == 0).sum();
     println!("{}", sum);
 }
