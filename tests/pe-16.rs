f79a1cfb34f9870a69561bb47311e9ffba4b727f
Andreas Christianson <achristianson@pessimistic-it.com>
2021-05-29 22:53:54 -0500

    formatting

--- tests/pe-16.rs
+++ tests/pe-16.rs
@@ -1,6 +1,6 @@
 extern crate project_euler_rust;
-use project_euler_rust::strings::string_to_integer_sequence::to_int_sequence;
 use num::BigUint;
+use project_euler_rust::strings::string_to_integer_sequence::to_int_sequence;
 /*
 
 <p>2<sup>15</sup> = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.</p>
@@ -10,26 +10,26 @@ use num::BigUint;
 
 #[test]
 fn solution() {
-  let value = BigUint::new(vec!(2)).pow(1000);
-  let as_sequence = to_int_sequence(value.to_string());
-  let mut sum = 0;
+    let value = BigUint::new(vec![2]).pow(1000);
+    let as_sequence = to_int_sequence(value.to_string());
+    let mut sum = 0;
 
-  for digit in as_sequence {
-      sum += digit;
-  }
+    for digit in as_sequence {
+        sum += digit;
+    }
 
-  assert_eq!(1366, sum);
+    assert_eq!(1366, sum);
 }
 
 #[test]
 fn given() {
-  let value = BigUint::new(vec!(2)).pow(15);
-  let as_sequence = to_int_sequence(value.to_string());
-  let mut sum = 0;
+    let value = BigUint::new(vec![2]).pow(15);
+    let as_sequence = to_int_sequence(value.to_string());
+    let mut sum = 0;
 
-  for digit in as_sequence {
-      sum += digit;
-  }
+    for digit in as_sequence {
+        sum += digit;
+    }
 
-  assert_eq!(26, sum);
+    assert_eq!(26, sum);
 }
